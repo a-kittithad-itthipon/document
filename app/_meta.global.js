714cde506fc4f59c352bd0ce43b2f511db052f61
a-kittithad-itthipon <kittithad.itthipon@gmail.com>
2026-01-22 19:50:02 +0700

    initial document project

---
+++ app/_meta.global.js
@@ -0,0 +1,92 @@
+const system_deployment = {
+  "platform": {
+    items: {
+      "active-site": "",
+      "dashboard": "",
+    }
+  },
+  "feature": {
+    items: {
+      "require": "",
+      "upload": "",
+    }
+  },
+};
+
+const basicItems = {
+  "docker-engine": {
+    items: {
+      "introduction": "",
+      "installation": "",
+      "commands": {
+        items: {
+          "docker-img": "",
+          "docker-container": "",
+          "docker-run": "",
+          "docker-network": "",
+          "docker-compose": ""
+        }
+      },
+    }
+  },
+  "docker-desktop": "",
+};
+
+const techStackItems = {
+  html: "",
+  php: {
+    items: {
+      "php": "",
+      "laravel": "",
+    }
+  },
+  python: {
+    items: {
+      "flask": "",
+    }
+  },
+  nodejs: {
+    items: {
+      express: "",
+      next: "",
+    }
+  },
+};
+
+const databaseItems = {
+  mysql: "",
+  pgsql: "",
+};
+
+export default {
+  docs: {
+    type: "page",
+    title: "Documentation",
+    theme: {
+      copyPage: false,
+    },
+    items: {
+      index: "",
+      _1: {
+        type: "separator",
+        title: "Platform",
+      },
+      ...system_deployment,
+      _2: {
+        type: "separator",
+        title: "Docker",
+      },
+      ...basicItems,
+      _3: {
+        type: "separator",
+        title: "Deploy",
+      },
+      ...techStackItems,
+      _4: {
+        type: "separator",
+        title: "Databases",
+      },
+      ...databaseItems,
+    },
+  },
+};
---
+++ app/layout.jsx
@@ -0,0 +1,61 @@
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import { Prompt } from "next/font/google";
+import "./globals.css";
+
+export const metadata = {
+  title: {
+    template: "%s | ADOCS",
+    default: "ADOCS",
+  },
+};
+
+const navbar = (
+  <Navbar
+    logo={<b>ADOCS</b>}
+    projectLink="https://github.com/a-kittithad-itthipon/document.git"
+  />
+);
+
+const promptSans = Prompt({
+  variable: "--font-sans",
+  subsets: ["latin", "thai"],
+  weight: ["200","300","400","500","600","700","800","900"],
+});
+
+
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
+
+export default async function RootLayout({ children }) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+      className={promptSans.variable}
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body className="font-sans">
+        <Layout
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/a-kittithad-itthipon/document"
+          sidebar={{ defaultMenuCollapseLevel: 1, toggleButton: false }}
+          editLink={null}
+          // navigation={false}
+          
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  );
+}
