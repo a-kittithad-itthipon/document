7f90d7b025bfc04d32b4ab871101dae44a3fdf43
a-kittithad-itthipon <kittithad.itthipon@gmail.com>
2026-01-25 19:17:43 +0700

    add contect to tech stack and db

--- components/embed-youtube.jsx
+++ components/embed-youtube.jsx
@@ -3,7 +3,7 @@
 export function EmbedYoutube({ id }) {
   return (
     <iframe
-      className="aspect-video w-full rounded-2xl"
+      className="aspect-video w-full rounded-2xl h-auto mb-4"
       src={"https://www.youtube.com/embed/" + id}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
